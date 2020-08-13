9e6494b1f223fa2fc7f8b76579f0470d9eed8574
StefansArya <stefansarya1@gmail.com>
2020-08-13 19:55:26 +0700

    Update example and rename files

--- src/vw-myview/example.js
+++ src/vw-myview/example.js
@@ -1,29 +1,35 @@
 // https://www.npmjs.com/package/scarletsframe#initializedefine-model
-sf.model.for('example', function(self, root){
+sf.model("example", function (self, root) {
 	// Shared model properties
 	self.message = "Hello";
 	self.description = "Developers! ";
-	// self.$el = may contains array of related `sf-m` element
+
+	//> self.$el = may contains array of related `sf-m` elements
+	// self.$el.addClass("heya");
+	// console.log(self.$el[0]);
+
+	// To be used for @click
+	self.toPage2 = function () {
+		views.goto("/page2");
+	}
 
 	// Init something when <sf-m name="example"> placed on DOM
-	self.init = function(){
-		setTimeout(textAnimation, 2000);
+	self.init = function () {
+		setTimeout(descAnimation, 2000);
 	}
 
-	// Being used for @click
-	self.toPage2 = function(){
-		views.goto('/page2');
-	}
-
-	function textAnimation(){
-		var description = "Let's getting started!".split('');
-
-		// Text animation
-		var interval = setInterval(function(){
+	// Animate self.description
+	function descAnimation() {
+		var description = "Let's getting started!".split("");
+		var interval = setInterval(function () {
 			self.description += description.shift();
-
-			if(description.length === 0)
-				clearInterval(interval);
+			if (description.length === 0) clearInterval(interval);
 		}, 50);
+	}
+
+	// Run something when this model script was reloaded
+	self.hotReloaded = function () {
+		self.description += "\n Again.. ";
+		descAnimation();
 	}
 });
